631fe393d4ea66d79ddb5532f203b8cd9cc1809f
Shreya Prasad <shreyateeza@gmail.com>
2020-09-06 13:25:56 +0530

    Day 11 files upload

---
+++ Day11/spring-core-demo3/src/main/java/com/manipal/spring_core_demo3/model/Employee.java
@@ -0,0 +1,45 @@
+package com.manipal.spring_core_demo3.model;
+
+public class Employee {
+	private long empId;
+	private String empName; 
+	private double salary;
+	private Address	address;
+	
+	
+	
+	public Employee(long empId, String empName, double salary, Address address) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.salary = salary;
+		this.address = address;
+	}
+	public long getEmpId() {
+		return empId;
+	}
+	public void setEmpId(long empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	} 
+	
+	
+	
+}
